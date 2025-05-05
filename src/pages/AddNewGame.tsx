2f392eafbbcf843386d42684f92f130e099e6ef2
Khwaja7 <borgodoff@gmail.com>
2025-05-05 21:59:42 +0200

    Cleaning refacto

--- src/pages/AddNewGame.tsx
+++ src/pages/AddNewGame.tsx
@@ -1,25 +1,9 @@
-import {
-  Button,
-  Checkbox,
-  CheckboxGroup,
-  Code,
-  createListCollection,
-  Fieldset,
-  Heading,
-  Portal,
-  Select,
-} from '@chakra-ui/react';
+import { Button, Checkbox, CheckboxGroup, Code, Fieldset, Heading, Stack } from '@chakra-ui/react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useController, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { HiOutlinePlus } from 'react-icons/hi';
-
-const guilds = createListCollection({
-  items: [
-    { label: 'SNEK', value: 'snek' },
-    { label: 'TestServer', value: 'test-server' },
-  ],
-});
+import ServerSelector from '../components/ServerSelector.tsx';
 
 const formSchema = z.object({
   game: z.array(z.string()).min(1, {
@@ -56,64 +40,44 @@ const AddNewGame = () => {
   return (
     <>
       <Heading size="2xl" margin="0 0 2rem 0">Add a new game</Heading>
-      <Select.Root collection={guilds} size="sm" width="320px">
-        <Select.HiddenSelect />
-        <Select.Label>Select your server</Select.Label>
-        <Select.Control>
-          <Select.Trigger>
-            <Select.ValueText placeholder="Select server" />
-          </Select.Trigger>
-          <Select.IndicatorGroup>
-            <Select.Indicator />
-          </Select.IndicatorGroup>
-        </Select.Control>
-        <Portal>
-          <Select.Positioner>
-            <Select.Content>
-              {guilds.items.map((guild) => (
-                <Select.Item item={guild} key={guild.value}>
-                  {guild.label}
-                  <Select.ItemIndicator />
-                </Select.Item>
-              ))}
-            </Select.Content>
-          </Select.Positioner>
-        </Portal>
-      </Select.Root>
-      
-      <form onSubmit={handleSubmit((data) => console.log(data))}>
-        <Fieldset.Root invalid={invalid}>
-          <Fieldset.Legend>Select your games</Fieldset.Legend>
-          <CheckboxGroup
-            invalid={invalid}
-            value={games.field.value}
-            onValueChange={games.field.onChange}
-            name={games.field.name}
-          >
-            <Fieldset.Content>
-              {items.map((item) => (
-                <Checkbox.Root key={item.value} value={item.value}>
-                  <Checkbox.HiddenInput />
-                  <Checkbox.Control>
-                    <HiOutlinePlus />
-                  </Checkbox.Control>
-                  <Checkbox.Label>{item.label}</Checkbox.Label>
-                </Checkbox.Root>
-              ))}
-            </Fieldset.Content>
-          </CheckboxGroup>
 
-          {errors.game && (
-            <Fieldset.ErrorText>{errors.game.message}</Fieldset.ErrorText>
-          )}
+      <ServerSelector></ServerSelector>
 
-          <Button size="sm" type="submit" alignSelf="flex-start">
-            Submit
-          </Button>
+      <Stack margin="2rem 0 0 0">
+        <form onSubmit={handleSubmit((data) => console.log(data))}>
+          <Fieldset.Root invalid={invalid}>
+            <Fieldset.Legend>Select your games</Fieldset.Legend>
+            <CheckboxGroup
+              invalid={invalid}
+              value={games.field.value}
+              onValueChange={games.field.onChange}
+              name={games.field.name}
+            >
+              <Fieldset.Content>
+                {items.map((item) => (
+                  <Checkbox.Root key={item.value} value={item.value}>
+                    <Checkbox.HiddenInput />
+                    <Checkbox.Control>
+                      <HiOutlinePlus />
+                    </Checkbox.Control>
+                    <Checkbox.Label>{item.label}</Checkbox.Label>
+                  </Checkbox.Root>
+                ))}
+              </Fieldset.Content>
+            </CheckboxGroup>
 
-          <Code>Values: {JSON.stringify(games.field.value, null, 2)}</Code>
-        </Fieldset.Root>
-      </form>
+            {errors.game && (
+              <Fieldset.ErrorText>{errors.game.message}</Fieldset.ErrorText>
+            )}
+
+            <Button size="sm" type="submit" alignSelf="flex-start">
+              Submit
+            </Button>
+
+            <Code>Values: {JSON.stringify(games.field.value, null, 2)}</Code>
+          </Fieldset.Root>
+        </form>
+      </Stack>
     </>
   );
 };
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ const Dashboard = () => {
   return (
     <>
       <Heading size="2xl">Dashboard</Heading>
-      <Stack gap="6" maxW="xs" margin="2em 0 0 0">
+      <Stack gap="6" maxW="xs" margin="2rem 0 0 0">
         <HStack width="full">
           <SkeletonCircle size="10" />
           <SkeletonText noOfLines={2} />
